bb8017d97cf5c3d57b6b2e4295aee3dc11b2027f
SarvarAnorboyev <sarvaranorboyev95@gmail.com>
2023-03-31 17:15:21 +0500

    fix

---
+++ index.js
@@ -0,0 +1,56 @@
+// let a = +prompt('a sonini kiriting')
+// let b = +prompt('b sonini kiriting')
+
+// function love6(a,b){
+//     if(a==6 || b==6){
+//         return(true)
+//     }else if(a+b == 6){
+//         return(true)
+//     }else{
+//         return(false)
+//     }
+// }
+// ---------------------------  -------------------------------------//
+// alert(love6(a,b)
+// var a = prompt('malumot kiriting')
+
+// function front3(a){
+//     return(a+a+a)
+// }
+
+// alert(front3(a))
+// --------------------------------- ----------------------//
+
+
+ 
+// let a= prompt("ismni kiriting");
+
+
+// function qaytaruvchi(a) {
+//     alert(a + a + a);
+
+// }
+
+// qaytaruvchi(a);
+
+
+// -----------------------------------------//
+
+// let a= + prompt("1-son");
+// let b = + prompt("2-son");
+// function sonnioling(a,b) {
+
+//     if(a==6 || b==6){
+//         alert("true");
+//     }else if (a+b == 6){
+//         alert("true")
+//     }else if (a-b == 6){
+//     alert("true")
+//     }else{
+//         alert("false")
+//     }
+// }
+
+// sonnioling(a,b);
+
+
